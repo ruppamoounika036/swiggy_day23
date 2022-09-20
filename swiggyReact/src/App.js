7040b490b48af2946a84a3543b07a44b196133b0
Aishwarya <aishwarya.guduru@techigai.io>
2022-09-20 14:50:55 +0530

    email added

--- swiggyReact/src/App.js
+++ swiggyReact/src/App.js
@@ -4,72 +4,12 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import ContactUs from './email.js'
 
-import { useState } from 'react';
-import { send } from 'emailjs-com';
-
 function App() {
 
-  const [toSend, setToSend] = useState({
-    from_name: '',
-    to_name: '',
-    message: '',
-    reply_to: '',
-  });
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    send(
-      'service_5nq44wr', 'template_tpq4i3c',
-      toSend,
-      'KHLQuaAFa0JltxlpR'
-    )
-      .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
-      })
-      .catch((err) => {
-        console.log('FAILED...', err);
-      });
-  };
-
-  const handleChange = (e) => {
-    setToSend({ ...toSend, [e.target.name]: e.target.value });
-  };
-
-  return (
-    <div className="App">
-            <form onSubmit={onSubmit}>
-        <input
-          type='text'
-          name='from_name'
-          placeholder='from name'
-          value={toSend.from_name}
-          onChange={handleChange}
-        />
-        <input
-          type='text'
-          name='to_name'
-          placeholder='to name'
-          value={toSend.to_name}
-          onChange={handleChange}
-        />
-        <input
-          type='text'
-          name='message'
-          placeholder='Your message'
-          value={toSend.message}
-          onChange={handleChange}
-        />
-        <input
-          type='text'
-          name='reply_to'
-          placeholder='Your email'
-          value={toSend.reply_to}
-          onChange={handleChange}
-        />
-         <button type='submit'>Submit</button>
-      </form>
-    </div>
-  );
+return(
+  <ContactUs/>
+);
+ 
 }
 
 export default App;
--- swiggyReact/src/email.js
+++ swiggyReact/src/email.js
@@ -1,31 +1,79 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
+import { useState } from 'react';
+import { send } from 'emailjs-com';
 
 export default function ContactUs() {
 
-  function sendEmail(e) {
-    e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
 
-    emailjs.sendForm('service_5nq44wr', 'template_tpq4i3c', e.target, 'KHLQuaAFa0JltxlpR')
-      .then((result) => {
-          window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
-      }, (error) => {
-          console.log(error.text);
+    const [toSend, setToSend] = useState({
+        from_name: '',
+        to_name: '',
+        to_mail:'',
+        message: '',
+        reply_to: '',
       });
-  }
-
-  return (
-    <form className="contact-form" onSubmit={sendEmail}>
-      <input type="hidden" name="contact_number" />
-      <label>Name</label>
-      <input type="text" name="from_name" />
-      <label>Email</label>
-      <input type="email" name="from_email" />
-      <label>Subject</label>
-      <input type="text" name="subject" />
-      <label>Message</label>
-      <textarea name="html_message" />
-      <input type="submit" value="Send" />
-    </form>
-  );
+    
+      const onSubmit = (e) => {
+        e.preventDefault();
+        send(
+          'service_5nq44wr', 'template_tpq4i3c',
+          toSend,
+          'KHLQuaAFa0JltxlpR'
+        )
+          .then((response) => {
+            console.log('SUCCESS!', response.status, response.text);
+          })
+          .catch((err) => {
+            console.log('FAILED...', err);
+          });
+      };
+    
+      const handleChange = (e) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+      };
+    
+      return (
+        <div className="App">
+                <form onSubmit={onSubmit}>
+            <input
+              type='text'
+              name='from_name'
+              placeholder='from name'
+              value={toSend.from_name}
+              onChange={handleChange}
+            />
+            <input
+              type='text'
+              name='to_mail'
+              placeholder='to email'
+              value={toSend.to_mail}
+              onChange={handleChange}
+            />
+            <input
+              type='text'
+              name='to_name'
+              placeholder='to name'
+              value={toSend.to_name}
+              onChange={handleChange}
+            />
+            <input
+              type='text'
+              name='message'
+              placeholder='Your message'
+              value={toSend.message}
+              onChange={handleChange}
+            />
+            <input
+              type='text'
+              name='reply_to'
+              placeholder='reply email'
+              value={toSend.reply_to}
+              onChange={handleChange}
+            />
+             <button type='submit'>Submit</button>
+          </form>
+        </div>
+      );
+ 
 }
